e4f8f2b42814560037308422ae62b8f91569d243
lamiyaaboobakkar <66775164+lamiyaaboobakkar@users.noreply.github.com>
2020-06-30 17:30:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 version = "~> 2.0"
-$ export AWS_ACCESS_KEY_ID
-$ export AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
 #access_key = var.AWS_ACCESS_KEY
 #secret_key = var.AWS_SECRET_KEY
 region = var.AWS_REGION
