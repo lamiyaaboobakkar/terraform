a4d6ac0e5f99febc03a6de0ff53bcf0f6d2e5b3d
lamiyaaboobakkar <66775164+lamiyaaboobakkar@users.noreply.github.com>
2020-06-30 17:03:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "lamiyabucket"
     key    = "path/terraform.tfstate"
     region = "ap-south-1"
-    #role_arn = "arn:aws:iam::553812159679:role/backends3role"
+    role_arn = "arn:aws:iam::553812159679:role/backends3role"
   }
 }
